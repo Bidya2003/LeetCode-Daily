22bf7f6423c837bba1988cacd529895a00bb1d48
Bidyajani Maji <bidyajanimaji2003@gmail.com>
2026-08-31 09:42:20 +0530

    Time: 5 ms (82.17%), Space: 45 MB (99.85%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+            return false;
+        
+        int rev = 0;
+        int num = x;
+
+        while(num!=0){
+            int mod = num%10;
+            rev = rev*10 + mod;
+            num = num/10;
+        }
+
+        if(rev==x)
+            return true;
+        
+        return false;
+        
+    }
+}
