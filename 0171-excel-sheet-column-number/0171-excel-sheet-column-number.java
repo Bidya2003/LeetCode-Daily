4e898379d1e6d5dc8309be44c8f6e3d75812e342
Bidyajani Maji <bidyajanimaji2003@gmail.com>
2026-09-04 11:24:06 +0530

    Time: 1 ms (90.48%), Space: 43.3 MB (86.28%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -5,8 +5,8 @@ class Solution {
         int total = 0;
         while(n<columnTitle.length()){
             char c = columnTitle.charAt(n);
-            int num = ((int)c - 64)-1;
-            total = (total*26) + num + 1;
+            int num = ((int)c - 64);
+            total = (total*26) + num;
             n++;
         }
 
