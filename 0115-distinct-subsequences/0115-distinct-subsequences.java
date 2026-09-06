76e41e7078bedb903bce40b41a728028d9200ff1
Bidyajani Maji <bidyajanimaji2003@gmail.com>
2026-09-06 08:46:52 +0530

    Time: 20 ms (53.25%), Space: 54.5 MB (38.68%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -28,31 +28,28 @@ class Solution {
 */
 
 
+
 class Solution {
+    public int findAllSubSequences(String s, String t, int indexT, int indexS, int[][] dp) {
+        if(indexT == t.length())
+            return 1;
+        if(indexS == s.length())
+            return 0;
 
-    public int solve(String s, String t, int i, int j, int[][] dp) {
+        if(dp[indexT][indexS] != -1)
+            return dp[indexT][indexS];
 
-        if (i == t.length()) return 1;
-        if (j == s.length()) return 0;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int ans = solve(s, t, i, j + 1, dp);   // skip
-
-        if (s.charAt(j) == t.charAt(i)) {
-            ans += solve(s, t, i + 1, j + 1, dp); // take
+        int count = findAllSubSequences(s,t,indexT,indexS+1,dp);
+        if(s.charAt(indexS) == t.charAt(indexT)){
+            count += findAllSubSequences(s,t,indexT+1,indexS+1,dp);
         }
-
-        return dp[i][j] = ans;
+        return dp[indexT][indexS] = count;
     }
-
     public int numDistinct(String s, String t) {
-
-        if (t.length() > s.length()) return 0;
-
         int[][] dp = new int[t.length()][s.length()];
-        for (int[] row : dp) Arrays.fill(row, -1);
-
-        return solve(s, t, 0, 0, dp);
+        for(int[] arr : dp){
+            Arrays.fill(arr,-1);
+        }
+        return findAllSubSequences(s,t,0,0,dp);
     }
 }
