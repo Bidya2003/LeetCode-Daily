f549acb2eddfb898e78bffd0063a8336f5c06f46
Bidyajani Maji <bidyajanimaji2003@gmail.com>
2026-09-09 16:59:34 +0530

    Time: 42 ms (38.56%), Space: 144.6 MB (35.95%) - LeetHub

--- 2127-maximum-employees-to-be-invited-to-a-meeting/2127-maximum-employees-to-be-invited-to-a-meeting.java
+++ 2127-maximum-employees-to-be-invited-to-a-meeting/2127-maximum-employees-to-be-invited-to-a-meeting.java
@@ -1,91 +1,71 @@
 class Solution {
-
     public int maximumInvitations(int[] favorite) {
-
         int n = favorite.length;
 
         List<List<Integer>> graph = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++){
             graph.add(new ArrayList<>());
         }
 
         int[] indegree = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            indegree[favorite[i]]++;
+        for(int i=0;i<n;i++){
             graph.get(favorite[i]).add(i);
+            indegree[favorite[i]]++;
         }
 
+        int[] depth = new int[n];
+        Arrays.fill(depth,1);
+        
         Queue<Integer> q = new LinkedList<>();
-
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
                 q.add(i);
             }
         }
 
-        // longest chain ending at each node
-        int[] depth = new int[n];
-
-        Arrays.fill(depth, 1);
-
-        // Topological Sort
-        while (!q.isEmpty()) {
-
+        while(!q.isEmpty()){
             int front = q.remove();
 
             int next = favorite[front];
-
-            // front -> next
-            depth[next] = Math.max(depth[next], depth[front] + 1);
+            depth[next] = Math.max(depth[next], depth[front]+1);
 
             indegree[next]--;
 
-            if (indegree[next] == 0) {
+            if(indegree[next]==0){
                 q.add(next);
             }
         }
 
+        boolean[] visited = new boolean[n];
         int maxCycle = 0;
         int pairChains = 0;
+        for(int i=0;i<n;i++){
+            int current = i;
 
-        boolean[] visited = new boolean[n];
-
-        for (int i = 0; i < n; i++) {
-
-            // cycle-এর মধ্যে নেই
-            if (indegree[i] == 0 || visited[i]) {
+            if(indegree[current]==0 || visited[current]){
                 continue;
             }
 
-            int current = i;
-            int cycleLen = 0;
-
-            while (!visited[current]) {
-
+            int cycleLength = 0;
+            while(!visited[current]){
+                cycleLength++;
                 visited[current] = true;
-
-                cycleLen++;
-
                 current = favorite[current];
             }
 
-            if (cycleLen == 2) {
+            if(cycleLength == 2){
+                int a = current;
+                int b = favorite[current];
 
-                int a = i;
-                int b = favorite[i];
-
-                // দুই side-এর longest chain + 2 cycle nodes
                 pairChains += depth[a] + depth[b];
-
-            } else {
-
-                maxCycle = Math.max(maxCycle, cycleLen);
+            }
+            else{
+                maxCycle = Math.max(maxCycle,cycleLength);
             }
         }
 
-        return Math.max(maxCycle, pairChains);
+
+        return Math.max(maxCycle,pairChains);
     }
 }
-
