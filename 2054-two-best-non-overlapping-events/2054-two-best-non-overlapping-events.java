1ab85b89a4a1caa556c962d93fe26b655e3af8cb
Bidyajani Maji <bidyajanimaji2003@gmail.com>
2026-09-12 09:52:25 +0530

    Time: 83 ms (30.51%), Space: 220.2 MB (32.02%) - LeetHub

---
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.java
@@ -0,0 +1,48 @@
+class Solution {
+    int[][] dp;
+    int[] next;
+    public int findMaxTwoEvents(int[][] events, int idx, int atMost){
+        if(idx==events.length || atMost==0){
+            return 0;
+        }
+
+        if(dp[idx][atMost] != -1){
+            return dp[idx][atMost];
+        }
+
+        int skip = findMaxTwoEvents(events,idx+1,atMost);
+        int take = events[idx][2] + findMaxTwoEvents(events,next[idx],atMost-1);
+
+        return dp[idx][atMost] = Math.max(skip,take);
+    }
+    public int maxTwoEvents(int[][] events) {
+        Arrays.sort(events, (a,b)->Integer.compare(a[0],b[0]));
+
+        next = new int[events.length];
+        for(int i=0;i<events.length;i++){
+            int left = i+1;
+            int right = events.length-1;
+            int n = events.length;
+
+            while(left<=right){
+                int mid = left + (right - left) / 2;
+
+                if(events[mid][0] > events[i][1]){
+                    n = mid;
+                    right = mid-1;
+                }
+                else{
+                    left = mid+1;
+                }
+            }
+            next[i] = n;
+        }
+
+        dp = new int[events.length][3];
+        for(int i=0;i<events.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
+
+        return findMaxTwoEvents(events,0,2);
+    }
+}
