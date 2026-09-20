783b7e1f27da144b6edf36ea8a82ed574c627f3d
Bidyajani Maji <bidyajanimaji2003@gmail.com>
2026-09-20 10:01:22 +0530

    Time: 1 ms (99.62%), Space: 46.9 MB (11.58%) - LeetHub

---
+++ 3936-minimum-swaps-to-move-zeros-to-end/3936-minimum-swaps-to-move-zeros-to-end.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minimumSwaps(int[] nums) {
+        int count = 0;
+
+        int left = 0;
+        int right = nums.length-1;
+
+        while(left<right){
+            if(nums[right] == 0)
+                right--;
+            else if(nums[left] != 0)
+                left++;
+            else if(nums[right] != 0 && nums[left] == 0){
+                nums[left] = nums[right];
+                nums[right] = 0;
+                count++;
+                left++;
+                right--;
+            }
+        }
+
+        return count;
+    }
+}
