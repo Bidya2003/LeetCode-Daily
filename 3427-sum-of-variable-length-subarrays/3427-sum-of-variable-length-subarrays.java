40a93d23c2032bf8b6011971e2c5298d3c86e7d8
Bidyajani Maji <bidyajanimaji2003@gmail.com>
2026-09-25 12:33:09 +0530

    Time: 2 ms (2.54%), Space: 45.1 MB (82.49%) - LeetHub

---
+++ 3427-sum-of-variable-length-subarrays/3427-sum-of-variable-length-subarrays.java
@@ -0,0 +1,18 @@
+class Solution { 
+    public int subarraySum(int[] nums) { 
+        int ans = 0; 
+        
+        for(int i = 0; i < nums.length; i++){ 
+            int sum = 0; 
+            int start = Math.max(0, i - nums[i]); 
+ 
+            for(int j = start; j <= i; j++){ 
+                sum += nums[j]; 
+            }
+            
+            ans += sum;
+        } 
+ 
+        return ans; 
+    } 
+}
