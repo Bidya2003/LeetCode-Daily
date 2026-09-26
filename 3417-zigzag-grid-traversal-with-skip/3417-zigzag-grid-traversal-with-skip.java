882f98dfcddef926a61b46a66d7328fd52d011a2
Bidyajani Maji <bidyajanimaji2003@gmail.com>
2026-09-26 08:42:28 +0530

    Time: 1 ms (100%), Space: 47.1 MB (95.91%) - LeetHub

---
+++ 3417-zigzag-grid-traversal-with-skip/3417-zigzag-grid-traversal-with-skip.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> zigzagTraversal(int[][] grid) {
+        List<Integer> ans = new ArrayList<>();
+
+        for(int i=0;i<grid.length;i++){
+            if(i%2 == 0){
+                for(int j=0;j<grid[0].length;j++){
+                    if((i+j) % 2 == 0){
+                        ans.add(grid[i][j]);
+                    }
+                }
+            }
+            else{
+                for(int j=grid[0].length-1; j>=0; j--){
+                    if((i+j) % 2 == 0){
+                        ans.add(grid[i][j]);
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}
